1e7f64863e52e5b90ea39319146f74d10a51397f
Arthur Vinicius <thuur.vss@gmail.com>
2024-09-17 08:09:18 -0300

    criando projeto node.js

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { app } from "./app";
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: 3333,
+  })
+  .then(() => {
+    console.log(`🚀 HTTP Server Running! on: http://localhost:3000`);
+  });
