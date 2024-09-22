cdaae21bbda5f6e954fc79c70299772f5a213f35
Arthur Vinicius <thuur.vss@gmail.com>
2024-09-22 17:51:04 -0300

    controller de registro

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,6 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { prisma } from "./lib/prisma";
+import { appRoutes } from "./http/controllers/routes";
 
 export const app = fastify();
 
-app.post("/users", async (request, reply) => {
-  const bodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
-
-  const { name, email, password } = bodySchema.parse(request.body);
-
-  const newUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
-
-  return reply.status(201).send(newUser);
-});
+app.register(appRoutes);
